6da8b83cf91bd7d8535f2c102bfd347bdbca68f3
sayan sarkar <2003.sayansarkar21@gmail.com>
2022-09-29 15:57:21 +0530

    program commit

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("hello\n enter 2 number\n");
+scanf("%d",&a);
+printf("enter 2nd number\n");
+scanf("%d",&b);
+printf("sum is %d",a+b);
+return 0;
+}
